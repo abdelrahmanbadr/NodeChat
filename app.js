775a7f21e97f08dceaf674c6327c59a944bd1712
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2018-03-18 06:48:25 +0200

    registrater user to DB

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var port = process.env.PORT || 3000;
 
 
 
+
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Headers','*');
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var users = require('../models/users');
-
+const bcrypt = require('bcrypt');
 
 // /* GET users listing except current user. */
 router.get('/:currentUserid',(req,res,next)=>{
@@ -11,11 +11,16 @@ router.get('/:currentUserid',(req,res,next)=>{
     })
 });
 router.post('/',(req,res,next)=>{
-  users.adduser((req.body),(err,result)=>{
-      if(err)  res.json(err);
-      else res.json(result);
-  })
+    
+    bcrypt.hash(req.body.password, 10, function(err, hash) {
+        req.body.password = hash;
+        users.adduser((req.body),(err,result)=>{
+            if(err)  res.json(err);
+            else res.json(result);
+        })
+    });
 });
+
 
 
 module.exports = router;
